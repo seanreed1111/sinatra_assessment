b8f9a1f941ed8163f16cd85e37c49875f51633f4
Sean Reed <seanreed1111@gmail.com>
2014-01-28 08:08:00 -0500

    add bootstrap files

--- app.rb
+++ app.rb
@@ -7,12 +7,29 @@ class SpaceCatApp < Sinatra::Application
         erb :index  
     end
 
+    get '/spacecats/edit/:id'
+        @spacecat = Spacecat.get(params[:id])
+        erb :edit
+    end
+
     get '/spacecats/new' do
         @spacecats = SpaceCat.new
         erb :new
     end
 
+    get '/spacecats/:id' do
+        @spacecat = Spacecat.get(params[:id])
+        erb :show
+    end
     post '/spacecats' do
-        @spacecats = Spacecat.new(params[:spacecats])
+        @spacecat = Spacecat.new(params[:spacecats])
+        @spacecat.save
+        erb: edit
+    end
+
+
+    delete '/spacecats/:id do'
+    Spacecat.get(params[:id]).destroy
+    redirect 'spacecats'
     end
 end
